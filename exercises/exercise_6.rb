52f3eb23ccdda004b296c869b913aeec1655026d
Bita <bita.janzadeh@hotmail.com>
2022-02-01 11:34:41 -0500

    Complete all activities

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,16 +9,21 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store4 = Store.find_by(id: 4)
+@store5 = Store.find_by(id: 5)
+@store6 = Store.find_by(id: 6)
+
+
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "Iglesias", last_name: "Enrique", hourly_rate: 10)
+@store1.employees.create(first_name: "Iglesias", last_name: "Enrique", hourly_rate: 42)
 @store2.employees.create(first_name: "Janzadeh", last_name: "Bita", hourly_rate: 120)
 @store2.employees.create(first_name: "Shakira", last_name: "Shakira", hourly_rate: 90)
-@store3.employees.create(first_name: "Lamb", last_name: "Mary", hourly_rate: 29)
-@store3.employees.create(first_name: "Man", last_name: "Muffin", hourly_rate: 26)
-@store4.employees.create(first_name: "Aardvark", last_name: "Arthur", hourly_rate: 16)
-@store4.employees.create(first_name: "Reed", last_name: "DW", hourly_rate: 7)
+@store2.employees.create(first_name: "Lamb", last_name: "Mary", hourly_rate: 122)
+@store2.employees.create(first_name: "Man", last_name: "Muffin", hourly_rate: 46)
+@store4.employees.create(first_name: "Aardvark", last_name: "Arthur", hourly_rate: 46)
+@store4.employees.create(first_name: "Reed", last_name: "DW", hourly_rate: 47)
 @store5.employees.create(first_name: "Felton", last_name: "Tom", hourly_rate: 80)
-@store5.employees.create(first_name: "Phelps", last_name: "Oliver", hourly_rate: 39)
-@store6.employees.create(first_name: "Phelps", last_name: "James", hourly_rate: 39)
-@store6.employees.create(first_name: "Snape", last_name: "Severus", hourly_rate: 39)
+@store5.employees.create(first_name: "Phelps", last_name: "Oliver", hourly_rate: 49)
+@store6.employees.create(first_name: "Phelps", last_name: "James", hourly_rate: 49)
+@store6.employees.create(first_name: "Snape", last_name: "Severus", hourly_rate: 59)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "What is your store name? >"
+store_name = gets.chomp
+
+@store7 = Store.create(name: store_name)
+puts @store7.errors.details
