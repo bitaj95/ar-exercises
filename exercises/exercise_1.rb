7569d1ea4da123bfb8dbfe60654d1fdfa845d5a0
Bita <bita.janzadeh@hotmail.com>
2022-02-01 00:02:17 -0500

    Complete first activity

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,10 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+
+burnaby = Store.create(name:"Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
+richmond = Store.create(name:"Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
+gastown = Store.create(name:"Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
+
+puts Store.count
